bc1fb04057797def363ae7a61bfec317ecca988a
Jonas Nell <jonasnell1705@gmail.com>
2026-05-27 18:34:13 +0200

    basic respo shit done

--- js/templates.js
+++ js/templates.js
@@ -1,11 +1,11 @@
 function getCategoryTemplate(category){
     return /*html*/`
         <section>
+            <div class="dish-header">
+                <h2>${category.title}</h2>
+                <img class="dish-icon" src="${category.icon}" alt="">
+            </div>
             <div class="content">
-                <div class="dish-header">
-                    <h2>${category.title}</h2>
-                    <img class="dish-icon" src="${category.icon}" alt="">
-                </div>
                 <div class="dish-container" id="${category.type}-container"></div>
             </div>
         </section>
